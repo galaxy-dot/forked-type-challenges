baab3f309120a07b3a69d260aee63ccd934a7eaa
Wendy <52882034+galaxy-dot@users.noreply.github.com>
2022-07-22 20:55:14 +1000

    11,14

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,10 @@
-type TupleToObject<T extends readonly any[]> = any
+/**
+ * 2022-07-22
+ * term:type
+ * ideation:
+ * First get each element of T and use is as the property key name of the new type
+ * The value is the K
+ * https://stackoverflow.com/questions/59187941/whats-the-tnumber-mean-in-typescript-code
+ * 又有点忘了in是啥了。。
+ */
+type TupleToObject<T extends readonly any[]> = { [ K in T[number]]: K }
--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,6 @@
-type First<T extends any[]> = any
+/**
+ * 2022-07-22
+ * term:type
+ * ideation:
+ */
+type First<T extends any[]> = T extends [] ? never : T[0]
